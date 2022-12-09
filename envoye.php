5ce5d894f1be8fd3bc34504e596a16d8bf44bc3e
Tarek Mokadem <mokadem59200@live.fr>
2022-12-09 22:57:16 +0100

    ajout des modif

--- envoye.php
+++ envoye.php
@@ -6,7 +6,7 @@ $headers = 'From: webmaster@example.com' . "\r\n" .
     'Reply-To: webmaster@example.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 echo $_GET['sujet'] $_GET['message'] $_GET['nom'] $_GET['prenom']
-mail($to, $subject, $message,$headers);
+mail($to, $subject, $message);
 echo '<html>';
 echo '<body>';
 echo '<p>test</p>';
@@ -24,37 +24,6 @@ echo '</html>';
     <title>STI Technology</title>
 </head>
 <body>
-    <a href="index.html" class="logo"><img src="stilogo.png" /></a>
-    <nav>
-      <ul class="menu">
-        <li><a href="#">Accueil</a>
-        </li>
-        <li><a href="#">Nos services</a>
-        </li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">A propos</a></li>
-      </ul>
-    </nav>
-    
-    <contenu>
-      <p class="valide"> Mail envoyé avec succès </p>
-    </contenu>
-    <footer>
-      <table class="gauche">
-        <tr>
-          <td class="logo">
-            <img src="stilogo.png"/>
-          </td>
-          <td class="coordonnee">
-            <img src="adresse.png"/><b>106, rue de Mouvaux 59200 TOURCOING </b><br>
-            <p><img src="rcs.png"/><b>R.C.S. Roubaix – Tourcoing :</b> 517 866 075 00016 </p> <br>
-            <p><img src="phone.png"/><b>Tél. : </b><a href="tel:" class="tel"> 09 20 70 34 43</a></p><br>
-            <p><img src="phone.png"/><b>Fax :</b><a href="tel:" class="tel"> 09 56 53 36 47</a></p>
-          </td>
-          <td><a href="#" class="foot">Contactez-nous</a></td>
-        </tr>
-      </table><br>
-      <p style="color: white;">@ 2022 STI Technology Copyright Ihssane</p> 
-    </footer>
+    <h1>Mail envoyé</h1>
 </body>
 </html>
